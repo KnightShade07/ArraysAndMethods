df25bd135d3eac9d5d51dd734419b65bdd9308e9
TemplarFalchion75 <ethen.oliva@yahoo.com>
2020-06-18 19:55:21 -0700

    Add project files.

---
+++ ArraysAndMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Transactions;
+
+namespace ArraysAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            enterNumberList();
+            copyArray();
+            displayArray();
+        }
+
+        public static void enterNumberList()
+        {
+            int test = 0;
+
+
+        }
+
+        public static int[]copyArray()
+        {
+            return copyArray();
+        }
+
+        public  static int[] displayArray(/*Takes the two arrays as parameters.*/) //Test
+        {
+            return displayArray();
+        }
+    }
+}
